8ac884b2d18dd2837c4ca8ee265be4b7c55ac1db
Mauro Oviedo <mauro@zipari.com>
2018-05-12 19:24:43 -0400

    Added normalize.css & babel to config. Updated reload config.

--- brunch-config.js
+++ brunch-config.js
@@ -3,3 +3,18 @@ exports.files = {
   javascripts: {joinTo: 'app.js'},
   stylesheets: {joinTo: 'app.css'}
 };
+
+exports.npm = {
+  styles: {'normalize.css': ['normalize.css']}
+};
+
+exports.plugins = {
+  babel: {presets: ['latest']},
+  autoReload: {
+    enabled: {
+      css: true,
+      js: true,
+      assets: true
+    }
+  }
+};
