5529b5fc99d1112ae0d2d68f6f39cd2d90dc1124
Mauro Oviedo <mauro@zipari.com>
2018-05-12 19:26:01 -0400

    Initial logic for game.

---
+++ app/components/game.js
@@ -0,0 +1,57 @@
+import Button from './button';
+
+function createGird(grid, size) {
+  for (let i = 0; i < size; i++) {
+    grid.push([]);
+  }
+}
+
+function checkVerticals(column, size) {
+  let has_x_in_a_row = false;
+
+  return has_x_in_a_row;
+}
+
+function checkHorizontals(row, size) {
+  let has_x_in_a_row = false;
+
+  return has_x_in_a_row;
+}
+
+function checkDiagnols(diagnol, size) {
+  let has_x_in_a_row = false;
+
+  return has_x_in_a_row;
+}
+
+class Game {
+  constructor(gridSize) {
+    this.grid = [];
+    this.size = gridSize;
+    this.markedSpaces = [];
+
+    createGird(this.grid, gridSize);
+  }
+
+  reset() {
+    this.grid = [];
+    createGird(this.grid, this.size);
+
+    this.markedSpaces.forEach(el => {
+      Button.reset(el);
+    });
+  }
+
+  markSpace(el) {
+    this.markedSpaces.push(el);
+    this.grid[el.dataset.row][el.dataset.column];
+  }
+
+  hasPalyerWon() {
+
+
+
+  }
+}
+
+export default Game;
